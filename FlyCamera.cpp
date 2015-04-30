3356b9f0682b28d355378d09ba9ad91dfb9b2fa0
Freeman Hsu <freeman@zhenhai.com.tw>
2015-04-30 10:15:08 +0800

    [2015/04/30]     1. add function - sharpness

--- FlyCamera.cpp
+++ FlyCamera.cpp
@@ -23,35 +23,42 @@ Camera cam;
 static const unsigned int sk_numProps = 18;
 
 const char* expo_title = "自動曝光 Off/On";
-const char* expo_value = "自動曝光值";
+const char* expo_value = "手動曝光值";
+const char* shar_title = "自動影像銳利化 Off/On";
+const char* shar_value = "手動影像銳利化值";
 const char* shut_title = "自動快門 Off/On";
-const char* shut_value = "快門值";
+const char* shut_value = "手動快門值";
 const char* bina_title = "影像二元化 Off/ On";
 const char* bina_max = "影像二元化最大接受閥值(+150)"; // binarization max value will between 0(+150) ~ 150(+150)
 const char* bina_thresh = "影像二元化閥值";      // binarization thresh will between 0 ~ 150
-const char* tess_title = "文字辨識 trigger";
+const char* tess_title = "單張文字辨識 trigger";
 
-int exposureOnOff = 0;
+int exposureOnOff = 0;         // exposure
 int exposureValue = 0;
 int oldExposureValue = 0;
-int shutterOnOff = 0;
+int sharpnessOnOff = 0;        // sharpness
+int sharpnessValue = 3000;
+int oldSharpnessValue = 3000;
+int shutterOnOff = 0;          // shutter
 int shutterValue = 0;
 int oldShutterValue = 0;
-int binaryOnOff = 0;
-int binaryMax =  100; // binarization max value will between 0(+150) ~ 150(+150), p.s. actually value should plus 150, so 150~300
+int binaryOnOff = 0;           // binarization
+int binaryMax =  100;          // binarization max value will between 0(+150) ~ 150(+150), p.s. actually value should plus 150, so 150~300
 int oldBinaryMax = 100;
 int binaryThresh = 30;
 int oldBinaryThresh = 30;
-int ocrOnOff = 0;
+int ocrOnOff = 0;              // OCR
 
-void on_slider_exposureOnOff(int, void*);
+void on_slider_exposureOnOff(int, void*);  // exposure
 void on_slider_exposureValue(int, void*);
-void on_slider_shutterOnOff(int, void*);
+void on_slider_sharpnessOnOff(int, void*); // sharpness
+void on_slider_sharpnessValue(int, void*);
+void on_slider_shutterOnOff(int, void*);   // shutter
 void on_slider_shutterValue(int, void*);
-void on_slider_binaryOnOff(int, void*);
+void on_slider_binaryOnOff(int, void*);    // binarization
 void on_slider_binaryMax(int, void*);
 void on_slider_binaryThresh(int, void*);
-void on_slider_ocrOnOff(int, void*);
+void on_slider_ocrOnOff(int, void*);       // OCR
 void OCR();
 
 void PrintBuildInfo() {
@@ -249,6 +256,8 @@ int main() {
     // Setup trackbar
     createTrackbar(expo_title, win_setting, &exposureOnOff, 1, on_slider_exposureOnOff);
     createTrackbar(expo_value, win_setting, &exposureValue, 1023, on_slider_exposureValue);
+    createTrackbar(shar_title, win_setting, &sharpnessOnOff, 1, on_slider_sharpnessOnOff);
+    createTrackbar(shar_value, win_setting, &sharpnessValue, 4095, on_slider_sharpnessValue);
     createTrackbar(shut_title, win_setting, &shutterOnOff, 1, on_slider_shutterOnOff);
     createTrackbar(shut_value, win_setting, &shutterValue, 1590, on_slider_shutterValue);
     createTrackbar(bina_title, win_opencv, &binaryOnOff, 1, on_slider_binaryOnOff);
@@ -268,6 +277,7 @@ int main() {
     return 0;
 }
 
+// EXPOSURE -start----------------------------
 void on_slider_exposureOnOff(int, void*) {
     Error error;
     Property prop;
@@ -311,7 +321,55 @@ void on_slider_exposureValue(int, void*) {
         }
     }
 }
+// EXPOSURE -end------------------------------
 
+// SHARPNESS -start---------------------------
+void on_slider_sharpnessOnOff(int, void*) {
+    Error error;
+    Property prop;
+    prop.type = AUTO_EXPOSURE;
+    error = cam.GetProperty(&prop);
+    if ( error != PGRERROR_OK) {
+        PrintError( error );
+    }
+
+    prop.absControl = false;
+    prop.onOff = true;
+    prop.autoManualMode = sharpnessOnOff; 
+    error = cam.SetProperty(&prop, false);
+    if ( error != PGRERROR_OK ) {
+        PrintError ( error );
+    }
+}
+
+void on_slider_sharpnessValue(int, void*) {
+    if (sharpnessOnOff == 1) { // AUTO mode
+        setTrackbarPos(shar_value, win_setting, oldSharpnessValue);
+    } else {
+        Error error;
+        Property prop;
+        prop.type = SHARPNESS;
+        error = cam.GetProperty(&prop);
+        if ( error != PGRERROR_OK) {
+            PrintError( error );
+        }
+
+        prop.absControl = false;
+        prop.onOff = true;
+        prop.autoManualMode = 0;
+        prop.valueA = sharpnessValue;
+        error = cam.SetProperty(&prop, false);
+        if ( error != PGRERROR_OK ) {
+            PrintError( error );
+            setTrackbarPos(shar_value, win_setting, oldSharpnessValue);
+        } else {
+            oldSharpnessValue = sharpnessValue;
+        }
+    }
+}
+// SHARPNESS -end-----------------------------
+
+// SHUTTER -start-----------------------------
 void on_slider_shutterOnOff(int, void*) {
     Error error;
     Property prop;
@@ -355,7 +413,9 @@ void on_slider_shutterValue(int, void*) {
         }
     }   
 }
+// SHUTTER -end-------------------------------
 
+// BINARIZATION -start------------------------
 void on_slider_binaryOnOff(int, void*) {}
 
 void on_slider_binaryMax(int, void*) {
@@ -373,7 +433,9 @@ void on_slider_binaryThresh(int, void*) {
         oldBinaryThresh = binaryThresh;
     }
 }
+// BINARIZATION -end--------------------------
 
+// OCR -start---------------------------------
 void on_slider_ocrOnOff(int, void*) {}
 
 void OCR() {
@@ -397,3 +459,4 @@ void OCR() {
         cout << "--------------------------" << endl;
     }
 }
+// OCR -end-----------------------------------
