cc08f766447c48c5347306060de9446fbb7e465f
freeman <freeman@zhenhai.com.tw>
2015-06-03 15:06:30 +0800

    [2015/06/03]     1. HoughtLine detect lines

--- LineAndCircle.cpp
+++ LineAndCircle.cpp
@@ -226,6 +226,37 @@ int RunSingleCamera( PGRGuid guid ) {
             Mat tmp;
             image.copyTo(tmp);
             Canny(tmp, image, cannyThresh, (cannyMax+150), 3);
+
+
+            Mat cdst;
+            image.copyTo(cdst);
+            //cvtColor(image, cdst, CV_GRAY2BGR);
+#if 0
+            vector<Vec2f> lines;
+            HoughLines(image, lines, 1, CV_PI/180, 100, 0, 0);
+
+            for (size_t x = 0; x < lines.size(); x++) {
+                float rho = lines[x][0], theta = lines[x][1];
+                Point pt1, pt2;
+                double a = cos(theta), b = sin(theta);
+                double x0 = a * rho, y0 = b * rho;
+                pt1.x = cvRound( x0 + 1000*(-b) );
+                pt1.y = cvRound( y0 + 1000*(a) );
+                pt2.x = cvRound( x0 - 1000*(-b) );
+                pt2.y = cvRound( y0 - 1000*(a) );
+                line( cdst, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
+            }
+# else 
+            vector<Vec4i> lines;
+            HoughLinesP(image, lines, 1, CV_PI/180, 50, 80, 10 );
+            for( size_t i = 0; i < lines.size(); i++ ) {
+                Vec4i l = lines[i];
+                line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255,255,255), 3, CV_AA);
+            }
+#endif
+            imshow("detected lines", cdst);
+        } else {
+            destroyWindow("detected lines");
         }
 
         imshow(win_title, image);
