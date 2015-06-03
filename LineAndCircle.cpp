153127735609b29fe743f2ffb0e5de9cacce1ef1
freeman <freeman@zhenhai.com.tw>
2015-06-03 12:29:54 +0800

    [2015/06/03]     1. add Canny function

--- LineAndCircle.cpp
+++ LineAndCircle.cpp
@@ -21,16 +21,19 @@ const char* win_opencv = "OpenCV 設定";
 Camera cam;
 static const unsigned int sk_numProps = 18;
 
-const char* expo_title = "自動曝光 Off/On";
-const char* expo_value = "手動曝光值";
-const char* shar_title = "自動影像銳利化 Off/On";
-const char* shar_value = "手動影像銳利化值";
-const char* shut_title = "自動快門 Off/On";
-const char* shut_value = "手動快門值";
-const char* bina_title = "影像二元化 Off/On";
-const char* binv_title = "影像二元化反轉 Off/On";
-const char* bina_max = "影像二元化最大接受閥值(+150)"; // binarization max value will between 0(+150) ~ 150(+150)
-const char* bina_thresh = "影像二元化閥值";            // binarization thresh will between 0 ~ 150
+const char* expo_title =  "自動曝光 Off/On";
+const char* expo_value =  "手動曝光值";
+const char* shar_title =  "自動影像銳利化 Off/On";
+const char* shar_value =  "手動影像銳利化值";
+const char* shut_title =  "自動快門 Off/On";
+const char* shut_value =  "手動快門值";
+const char* bina_title =  "影像二元化 Off/On";
+const char* binv_title =  "影像二元化反轉 Off/On";
+const char* bina_max=     "影像二元化最大接受閥值(+150)"; // binarization max value will between 0(+150) ~ 150(+150)
+const char* bina_thresh = "影像二元化閥值";               // binarization thresh will between 0 ~ 150
+const char* cann_title = "Canny 測邊 Off/On";
+const char* cann_max   =  "Canny 測邊最大接受閥值(+150)";
+const char* cann_thresh = "Canny 測邊閥值";
 
 int exposureOnOff = 0;         // exposure
 int exposureValue = 0;
@@ -42,8 +45,13 @@ int binaryOnOff = 0;           // binarization
 int binaryInvOnOff = 0;        // binarization inverse
 int binaryMax =  100;          // binarization max value will between 0(+150) ~ 150(+150), p.s. actually value should plus 150, so 150~300
 int oldBinaryMax = 100;
-int binaryThresh = 30; 
-int oldBinaryThresh = 30; 
+int binaryThresh = 30;
+int oldBinaryThresh = 30;
+int cannyOnOff = 0;            // canny
+int cannyMax = 50;             // canny max value will between 0(150+) ~ 100(+150), p.s. actually value should plus 150, so 150 ~ 250
+int oldCannyMax = 50;
+int cannyThresh = 50;
+int oldCannyThresh = 50;
 
 void on_slider_exposureOnOff(int, void*);  // exposure
 void on_slider_exposureValue(int, void*);
@@ -53,6 +61,8 @@ void on_slider_shutterOnOff(int, void*);   // shutter
 void on_slider_shutterValue(int, void*);
 void on_slider_binaryMax(int, void*);      // binarization
 void on_slider_binaryThresh(int, void*);
+void on_slider_cannyMax(int, void*);       // canny
+void on_slider_cannyThresh(int, void*);
 
 void PrintError( FlyCapture2::Error error ) {
     error.PrintErrorTrace();
@@ -79,16 +89,19 @@ int main(int argc, char** argv)
     namedWindow(win_setting, WINDOW_NORMAL);
     namedWindow(win_opencv, WINDOW_NORMAL);
 
-    createTrackbar(expo_title, win_setting, &exposureOnOff, 1, on_slider_exposureOnOff);
-    createTrackbar(expo_value, win_setting, &exposureValue, 1023, on_slider_exposureValue);
-    createTrackbar(shar_title, win_setting, &sharpnessOnOff, 1, on_slider_sharpnessOnOff);
+    createTrackbar(expo_title, win_setting, &exposureOnOff,  1,    on_slider_exposureOnOff);
+    createTrackbar(expo_value, win_setting, &exposureValue,  1023, on_slider_exposureValue);
+    createTrackbar(shar_title, win_setting, &sharpnessOnOff, 1,    on_slider_sharpnessOnOff);
     createTrackbar(shar_value, win_setting, &sharpnessValue, 4095, on_slider_sharpnessValue);
-    createTrackbar(shut_title, win_setting, &shutterOnOff, 1, on_slider_shutterOnOff);
-    createTrackbar(shut_value, win_setting, &shutterValue, 1590, on_slider_shutterValue);
-    createTrackbar(bina_title, win_opencv, &binaryOnOff, 1);
-    createTrackbar(binv_title, win_opencv, &binaryInvOnOff, 1);
-    createTrackbar(bina_max, win_opencv, &binaryMax, 150, on_slider_binaryMax);
-    createTrackbar(bina_thresh, win_opencv, &binaryThresh, 150, on_slider_binaryThresh);
+    createTrackbar(shut_title, win_setting, &shutterOnOff,   1,    on_slider_shutterOnOff);
+    createTrackbar(shut_value, win_setting, &shutterValue,   1590, on_slider_shutterValue);
+    createTrackbar(bina_title, win_opencv,  &binaryOnOff,    1);
+    createTrackbar(binv_title, win_opencv,  &binaryInvOnOff, 1);
+    createTrackbar(bina_max,   win_opencv,  &binaryMax,      150,  on_slider_binaryMax);
+    createTrackbar(bina_thresh,win_opencv,  &binaryThresh,   150,  on_slider_binaryThresh);
+    createTrackbar(cann_title, win_opencv,  &cannyOnOff,     1);
+    createTrackbar(cann_max,   win_opencv,  &cannyMax,       150,  on_slider_cannyMax);
+    createTrackbar(cann_thresh,win_opencv,  &cannyThresh,     200,  on_slider_cannyThresh);
 
     for (unsigned int i=0; i < numCameras; i++) {
         PGRGuid guid;
@@ -207,6 +220,14 @@ int RunSingleCamera( PGRGuid guid ) {
                 threshold(image, image, binaryThresh, (binaryMax+150), CV_THRESH_BINARY);
             }
         }
+
+        // Canny
+        if (cannyOnOff == 1) {
+            Mat tmp;
+            image.copyTo(tmp);
+            Canny(tmp, image, cannyThresh, (cannyMax+150), 3);
+        }
+
         imshow(win_title, image);
     }            
 
@@ -317,3 +338,20 @@ void on_slider_binaryThresh(int, void*) {
     }
 }
 // BINARIZATION -end--------------------------
+// CANNY -start-------------------------------
+void on_slider_cannyMax(int, void*) {
+    if (cannyOnOff == 0) {
+        setTrackbarPos(cann_max, win_opencv, oldCannyMax);
+    } else {
+        oldCannyMax = cannyMax;
+    }
+}
+
+void on_slider_cannyThresh(int, void*) {
+    if (cannyOnOff == 0) {
+        setTrackbarPos(cann_thresh, win_opencv, oldCannyThresh);
+    } else {
+        oldCannyThresh = cannyThresh;
+    }
+}
+// CANNY -end---------------------------------
