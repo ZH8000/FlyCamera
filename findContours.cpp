72363b90f672cb42b4e44dc5a57fa49b249d008a
freeman <freeman@zhenhai.com.tw>
2015-07-23 17:16:34 +0800

    [2015/07/23]     1. temp

--- findContours.cpp
+++ findContours.cpp
@@ -58,15 +58,18 @@ void thresh_callback(int, void*) {
     Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
     for (int x=0; x<contours.size(); x++) {
         Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-        drawContours(drawing, contours, x, color, 1, 8, hierarchy, 0, Point());
+        drawContours(drawing, contours, x, color, CV_FILLED, 8, hierarchy, 0, Point());
     }
     namedWindow("Contours", CV_WINDOW_AUTOSIZE);
     imshow("Contours", drawing);
 
     // calculate the area with the moments 00 and compare with the result of the OpenCV function
+    float biggestArea = 0.00f;
     printf("\t Info: Area and Contour Length \n");
     for (int x=0; x<contours.size(); x++) {
         printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", x, mu[x].m00, contourArea(contours[x]), arcLength( contours[x], true ) );
+
+        //if (contourArea)
         //Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255));
         //drawContours(drawing, contours, x, color, 1, 8, hierarchy, 0, Point());
         //circle(drawing, mc[x], 4, color, -1, 8, 0);
